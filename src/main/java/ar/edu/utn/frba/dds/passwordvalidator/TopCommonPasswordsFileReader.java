144a7e3b483d3567e569acfd33c4bc0275613393
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-04-14 20:54:29 -0300

    :coffin: Remove unused import

--- src/main/java/ar/edu/utn/frba/dds/passwordvalidator/TopCommonPasswordsFileReader.java
+++ src/main/java/ar/edu/utn/frba/dds/passwordvalidator/TopCommonPasswordsFileReader.java
@@ -1,7 +1,6 @@
 package ar.edu.utn.frba.dds.passwordvalidator;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
