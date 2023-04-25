c6028467925a02facd764f2d866fc7d94d86604a
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-04-25 17:00:12 -0300

    :construction: Add special character to validation

--- src/main/java/ar/edu/utn/frba/dds/passwordvalidator/StrongPasswordValidation.java
+++ src/main/java/ar/edu/utn/frba/dds/passwordvalidator/StrongPasswordValidation.java
@@ -6,7 +6,7 @@ public class StrongPasswordValidation implements PasswordValidation {
   public void validatePassword(String password) {
     if (
         !password.matches(
-            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$")) {
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()|])(?=\\S+$).{8,20}$")) {
       throw new RuntimeException("The password is not strong enough.");
     }
   }
