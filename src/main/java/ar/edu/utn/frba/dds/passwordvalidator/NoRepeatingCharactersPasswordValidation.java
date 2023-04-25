4a5ba1c7fbed298932b3ebdb0991dd48549d8fdd
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-04-25 19:37:50 -0300

    :recycle: Simplify if statements

--- src/main/java/ar/edu/utn/frba/dds/passwordvalidator/NoRepeatingCharactersPasswordValidation.java
+++ src/main/java/ar/edu/utn/frba/dds/passwordvalidator/NoRepeatingCharactersPasswordValidation.java
@@ -3,11 +3,7 @@ package ar.edu.utn.frba.dds.passwordvalidator;
 public class NoRepeatingCharactersPasswordValidation implements PasswordValidation {
   @Override
   public boolean validatePassword(String password) {
-    if (
-        !password.matches(
-            "^(?!.*(\\w)\\1+).+$")) {
-      return false;
-    }
-    return true;
+    return password.matches(
+        "^(?!.*(\\w)\\1+).+$");
   }
 }
--- src/main/java/ar/edu/utn/frba/dds/passwordvalidator/StrongPasswordValidation.java
+++ src/main/java/ar/edu/utn/frba/dds/passwordvalidator/StrongPasswordValidation.java
@@ -4,11 +4,7 @@ public class StrongPasswordValidation implements PasswordValidation {
 
   @Override
   public boolean validatePassword(String password) {
-    if (
-        !password.matches(
-            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()|])(?=\\S+$).{8,20}$")) {
-      return false;
-    }
-    return true;
+    return password.matches(
+        "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()|])(?=\\S+$).{8,20}$");
   }
 }
