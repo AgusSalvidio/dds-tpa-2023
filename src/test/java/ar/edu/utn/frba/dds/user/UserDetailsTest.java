405f63f625702ccf592b3ee933d6cd207ca6ade3
Agustin Salvidio <agustinsalvidio@gmail.com>
2023-04-25 13:59:26 -0300

    :white_check_mark: Added some displays descriptions to tests

--- src/test/java/ar/edu/utn/frba/dds/user/UserDetailsTest.java
+++ src/test/java/ar/edu/utn/frba/dds/user/UserDetailsTest.java
@@ -1,27 +1,29 @@
 package ar.edu.utn.frba.dds.user;
 
 import ar.edu.utn.frba.dds.user.UserDetails;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 
 public class UserDetailsTest {
   @Test
+  @DisplayName("Create user details")
   public void createUserDetails() throws Exception {
-    UserDetails userDetails = UserDetails.composedBy("Hugo","Ibarra","ibarraneta@gmail.com");
+    UserDetails userDetails = UserDetails.composedBy("Hugo", "Ibarra", "ibarraneta@gmail.com");
 
-    Assertions.assertEquals("Hugo",userDetails.name());
-    Assertions.assertEquals("Ibarra",userDetails.lastname());
-    Assertions.assertEquals("ibarraneta@gmail.com",userDetails.email());
+    Assertions.assertEquals("Hugo", userDetails.name());
+    Assertions.assertEquals("Ibarra", userDetails.lastname());
+    Assertions.assertEquals("ibarraneta@gmail.com", userDetails.email());
   }
+
   @Test
+  @DisplayName("When creating an user details with any empty field should raise error")
   public void cannotCreateUserDetailsWhenAnyFieldIsEmpty() throws Exception {
-    Assertions.assertThrows(Exception.class,()-> UserDetails.composedBy("","Ibarra","ibarraneta@gmail.com"), "Los campos no pueden estar en blanco.");
-    Assertions.assertThrows(Exception.class,()-> UserDetails.composedBy("Hugo","","ibarraneta@gmail.com"),"Los campos no pueden estar en blanco.");
-    Assertions.assertThrows(Exception.class,()-> UserDetails.composedBy("Hugo","Ibarra",""),"Los campos no pueden estar en blanco.");
+    Assertions.assertThrows(Exception.class, () -> UserDetails.composedBy("", "Ibarra", "ibarraneta@gmail.com"), "Los campos no pueden estar en blanco.");
+    Assertions.assertThrows(Exception.class, () -> UserDetails.composedBy("Hugo", "", "ibarraneta@gmail.com"), "Los campos no pueden estar en blanco.");
+    Assertions.assertThrows(Exception.class, () -> UserDetails.composedBy("Hugo", "Ibarra", ""), "Los campos no pueden estar en blanco.");
   }
-
-
 
 
 }
