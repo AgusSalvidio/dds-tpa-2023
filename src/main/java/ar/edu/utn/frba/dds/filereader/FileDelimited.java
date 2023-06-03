aaea1fecb8599fd0353cb80e4da7e58d0aeab0b4
tum-vel-mishek <fdimatteo@frba.utn.edu.ar>
2023-06-02 23:14:09 -0300

    correccion en Merge

--- src/main/java/ar/edu/utn/frba/dds/filereader/FileDelimited.java
+++ src/main/java/ar/edu/utn/frba/dds/filereader/FileDelimited.java
@@ -11,27 +11,14 @@ public class FileDelimited extends DataFile {
   @Override
   public String[] parseLine() {
     try {
-<<<<<<< HEAD
       String[] fieldsAux = new String[this.fields.size()];
       if (this.bufferedReader != null) {
         fieldsAux = this.bufferedReader.readLine().split(this.getColDelimiter());
       }
       return fieldsAux;
-=======
-      String line;
-      if ((line = this.bufferedReader.readLine()) != null) {
-        String[] vfields = line.split(this.colDelimiter);
-        return vfields;
-      }
->>>>>>> 490afd623d14ae1a61e99c7d313f7add20e4692a
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-<<<<<<< HEAD
-
-=======
-    return new String[0];
->>>>>>> 490afd623d14ae1a61e99c7d313f7add20e4692a
   }
 
 }
