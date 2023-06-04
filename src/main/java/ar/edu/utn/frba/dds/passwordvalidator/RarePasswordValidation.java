f3e32b2280f23c48bbc10e0533c8c9ebcf88e84c
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-06-04 00:18:54 -0300

    :recycle: Implement password cache in rare password validation

--- src/main/java/ar/edu/utn/frba/dds/passwordvalidator/RarePasswordValidation.java
+++ src/main/java/ar/edu/utn/frba/dds/passwordvalidator/RarePasswordValidation.java
@@ -4,19 +4,28 @@ import java.io.IOException;
 
 public class RarePasswordValidation implements PasswordValidation {
   private final TopCommonPasswordsFileReader passwordsFileReader;
+  private PasswordCache passwordCache;
 
-  public RarePasswordValidation(TopCommonPasswordsFileReader passwordsFileReader) {
+  public RarePasswordValidation(
+      TopCommonPasswordsFileReader passwordsFileReader) {
     this.passwordsFileReader = passwordsFileReader;
+    this.passwordCache = new PasswordCache();
   }
 
   @Override
   public boolean validatePassword(String password) {
 
     try {
-      return passwordsFileReader.findPassword(password);
+      if (passwordCache.cacheAlreadyHas(password)) {
+        return false;
+      }
+      if (!passwordsFileReader.findPassword(password)) {
+        passwordCache.addNewPassword(password);
+        return false;
+      }
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-
+    return true;
   }
 }
