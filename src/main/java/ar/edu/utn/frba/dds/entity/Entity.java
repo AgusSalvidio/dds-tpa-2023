16f0c1bc094da289aed9828d9a4dd6da993897b3
Federico Fuentes Weber <fedefuentesweber@frba.utn.edu.ar>
2023-07-11 23:49:14 -0300

    :construction: Add AverageClosingTimeRanking class

--- src/main/java/ar/edu/utn/frba/dds/entity/Entity.java
+++ src/main/java/ar/edu/utn/frba/dds/entity/Entity.java
@@ -1,8 +1,10 @@
 package ar.edu.utn.frba.dds.entity;
 
 import ar.edu.utn.frba.dds.establishment.Establishment;
+import ar.edu.utn.frba.dds.incident.Incident;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,9 +17,12 @@ public abstract class Entity {
 
   public EntityName name;
   public List<Establishment> establishments;
+  public List<Incident> incidents;
 
   public Entity() {
+
     this.establishments = new ArrayList();
+    this.incidents = new ArrayList<>();
   }
 
   public void addNewEstablishment(Establishment newEstablishment) {
@@ -30,6 +35,35 @@ public abstract class Entity {
 
   public String name() {
     return this.name.getName();
+  }
+
+
+  public void addNewIncident(Incident newIncident) {
+    this.incidents.add(newIncident);
+  }
+
+  public List<Incident> incidents() {
+    return this.incidents;
+  }
+
+  public double averageClosingTimeForIncidents() {
+    OptionalDouble average;
+
+    average = this.incidents.stream().mapToDouble(Incident::closingTime).average();
+
+    return average.isPresent() ? average.getAsDouble() : 0;
+  }
+
+  public List<Incident> validIncidents() {
+    List<Incident> incidentsWithMoreThan24Hours = new ArrayList<>();
+
+    for (Incident incident : this.incidents) {
+      if (incident.reported24HoursAgo() && incident.state().name().equals("OPEN")) {
+        incidentsWithMoreThan24Hours.add(incident);
+      }
+    }
+
+    return incidentsWithMoreThan24Hours;
   }
 
 }
