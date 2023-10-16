196a8eeb2162cf8e5d148726e7e3e5bccc2c4210
Agustin Salvidio <agustinsalvidio@gmail.com>
2023-10-16 19:23:55 -0300

    :sparkles: Added Server and router classes to enhance app deployment and maintence

---
+++ src/main/java/ar/edu/utn/frba/dds/server/Server.java
@@ -0,0 +1,57 @@
+package ar.edu.utn.frba.dds.server;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import io.javalin.Javalin;
+import io.javalin.config.JavalinConfig;
+import io.javalin.http.HttpStatus;
+import io.javalin.rendering.JavalinRenderer;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+public class Server {
+  private static Javalin app = null;
+
+  public static Javalin app() {
+    if (app == null)
+      throw new RuntimeException("App no inicializada");
+    return app;
+  }
+
+  public static void initialize() {
+    if (app == null) {
+      Integer port = Integer.parseInt(System.getProperty("port", "8080"));
+      app = Javalin.create(config()).start(port);
+      initializeTemplateEngine();
+      Router.initialize();
+    }
+  }
+
+  private static void initializeTemplateEngine() {
+    JavalinRenderer.register(
+        (path, model, context) -> { // This renders the template
+          Handlebars handlebars = new Handlebars();
+          Template template = null;
+          try {
+            template = handlebars.compile("templates/" + path.replace(".hbs", ""));
+            return template.apply(model);
+          } catch (IOException e) {
+            //
+            e.printStackTrace();
+            context.status(HttpStatus.NOT_FOUND);
+            return "No se encuentra la página indicada...";
+          }
+        }, ".hbs" // Template file extension
+    );
+  }
+
+  private static Consumer<JavalinConfig> config() {
+    return config -> {
+      config.staticFiles.add(staticFiles -> {
+        staticFiles.hostedPath = "/";
+        staticFiles.directory = "/public";
+      });
+    };
+  }
+
+}
